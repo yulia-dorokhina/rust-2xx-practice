37be24d7131995679617cc70f009d99966da6066
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-09-27 12:06:43 +0300

    -- task 399

--- src/chapter3.rs
+++ src/chapter3.rs
@@ -7,3 +7,14 @@ fn test31() {
     assert_eq!(x, 5);
     println!("Success!");
 }
+
+#[test]
+fn test399() {
+    let x = 5;
+    let y = 10;
+
+    let real = x + y;
+    let expected = 15;
+
+    assert_eq!(real, expected);
+}
